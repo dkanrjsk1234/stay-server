69c583f5354e68ca32b360795832682a08b64bd1
qtaghdi <a01071397193@gmail.com>
2023-10-31 20:40:01 +0900

    change port to env

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const city = 'Seoul';
 const apiKey = '88696efc50f0ce92ae2a33fec6ab7645';
