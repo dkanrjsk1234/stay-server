0fe3ff2e6a19efc22cd4c7923895b89c5ffd6bd4
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2023-10-31 19:42:31 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
+const express = require('express');
 const axios = require('axios');
+const app = express();
+const port = 8080;
 
 const city = 'Seoul';
 const apiKey = '88696efc50f0ce92ae2a33fec6ab7645';
 const apiURI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-axios.get(apiURI)
-  .then(response => {
-    var data = response.data;
-    var temp = String((data.main.temp - 272)).substring(0, 3); // 현재 온도
-    var tempMax = String((data.main.temp_max - 272)).substring(0, 3); // 최고 온도
-    var tempMin = String((data.main.temp_min - 272)).substring(0, 3); // 최저 온도
-    var humidity = data.main.humidity; // 습도
-    var location = data.name; // 지역
-    console.log('지역 : ' + location + ' 현재 온도 : ' + temp + '도, 최고 온도 : ' + tempMax + '도, 최저 온도 : ' + tempMin + '도, 습도 : ' + humidity + '%');
-    // POST 요청을 보내는 부분
-    return axios.post('여기에 삽입', {
-      location: location,
-      temperature: temp,
-      maxTemperature: tempMax,
-      minTemperature: tempMin,
-      humidity: humidity
+app.get('/weather', (req, res) => {
+  axios.get(apiURI)
+    .then(response => {
+      var data = response.data;
+      var temp = String((data.main.temp - 273.15).toFixed(2));
+      var tempMax = String((data.main.temp_max - 273.15).toFixed(2));
+      var tempMin = String((data.main.temp_min - 273.15).toFixed(2));
+      var humidity = data.main.humidity;
+      var location = data.name;
+      res.send({
+        '지역': location,
+        '현재 온도': temp,
+        '최고 온도': tempMax,
+        '최저 온도': tempMin,
+        '습도': humidity +"%"
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error occurred while fetching weather data');
     });
-  })
-  .then(response => {
-    console.log('POST 완료');
-  })
-  .catch(error => {
-    console.error(error);
-  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
