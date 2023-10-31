7b171ef72a67c7b85c7e7d99fd591a7d207cd705
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2023-10-31 17:35:49 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,48 +1,30 @@
-const express = require('express');
 const axios = require('axios');
 
-const app = express();
-const port = 3000;
-
+const city = 'Seoul';
 const apiKey = '88696efc50f0ce92ae2a33fec6ab7645';
+const apiURI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-app.get('/weather', async (req, res) => {
-  try {
-    const { city } = req.query;
-
-    if (!city) {
-      return res.status(400).json({ error: 'City parameter is required' });
-    }
-
-    const weatherData = await getWeatherData(city);
-
-    res.json(weatherData);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-async function getWeatherData(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-  try {
-    const response = await axios.get(apiUrl);
-    const weatherData = response.data;
-
-    return {
-      city: weatherData.name,
-      temperature: weatherData.main.temp,
-      HighTemperature: weatherData.main.temp_max,
-      LowTemperature: weatherData.main.temp_min,
-      humidity: weatherData.main.humidity,
-      description: weatherData.weather[0].description,
-    };
-  } catch (error) {
-    throw new Error('Error fetching weather data from OpenWeather API');
-  }
-}
-
-app.listen(port, () => {
-  console.log(`Server address : http://localhost:${port}`);
-});
+axios.get(apiURI)
+  .then(response => {
+    var data = response.data;
+    var temp = String((data.main.temp - 272)).substring(0, 3); // 현재 온도
+    var tempMax = String((data.main.temp_max - 272)).substring(0, 3); // 최고 온도
+    var tempMin = String((data.main.temp_min - 272)).substring(0, 3); // 최저 온도
+    var humidity = data.main.humidity; // 습도
+    var location = data.name; // 지역
+    console.log('지역 : ' + location + ' 현재 온도 : ' + temp + '도, 최고 온도 : ' + tempMax + '도, 최저 온도 : ' + tempMin + '도, 습도 : ' + humidity + '%');
+    // POST 요청을 보내는 부분
+    return axios.post('http://example.com/your-endpoint', {
+      location: location,
+      temperature: temp,
+      maxTemperature: tempMax,
+      minTemperature: tempMin,
+      humidity: humidity
+    });
+  })
+  .then(response => {
+    console.log('POST 완료');
+  })
+  .catch(error => {
+    console.error(error);
+  });
